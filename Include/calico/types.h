0116e603e159802861672f27d0dedc5928cc4c55
negloir <mr.shamar@gmail.com>
2025-08-10 10:05:57 +0100

    Create types.h

---
+++ Include/calico/types.h
@@ -0,0 +1,12 @@
+#pragma once
+/* Minimal calico/types.h stub for CI */
+typedef signed char s8;
+typedef unsigned char u8;
+typedef signed short s16;
+typedef unsigned short u16;
+typedef signed int s32;
+typedef unsigned int u32;
+typedef long long s64;
+typedef unsigned long long u64;
+typedef float f32;
+typedef double f64;
