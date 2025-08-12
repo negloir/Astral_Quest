bbfa71f76c55c097325175bf5918629ec6459ebd
negloir <mr.shamar@gmail.com>
2025-08-12 13:14:12 +0100

    Update game.h

--- include/game.h
+++ include/game.h
@@ -1,36 +1,34 @@
 #pragma once
-#ifndef ARM9
-#define ARM9 1
-#endif
-#ifndef __NDS__
-#define __NDS__ 1
-#endif
+
 #include <nds.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
-#define GAME_TITLE "Astral Quest: Echoes of the Shards"
-#define VERSION_STR "0.1.0-dev"
-
-typedef struct { u16 hp, mp, atk, def, spd; u16 level, exp; } Stats;
+// ------------ Game state ------------
+typedef struct {
+    const char *name;
+    int hp;
+    int max_hp;
+    bool defending;
+} Entity;
 
 typedef struct {
-    char name[16];
-    Stats stats;
-    u32 rng_seed;
-} Player;
+    Entity player;
+    Entity enemy;
+    bool gameOver;
+    char lastMsg[96];
+} GameState;
 
-typedef struct {
-    char name[16];
-    Stats stats;
-    bool isBoss;
-} Enemy;
+// Global-ish state (lives in main.c, referenced from util.c)
+extern GameState g;
 
+// ------------ Game API ------------
 void game_init(void);
-void game_loop(void);
-void draw_hud(const Player* p, const Enemy* e, int turn);
-void draw_text_center(int y, const char* text);
-void wait_vblank_and_keys(u16 mask);
-u32 xorshift32(u32* state);
-int randint(u32* state, int lo, int hi);
-void save_profile(const Player* p);
-bool load_profile(Player* p);
+void game_update(void);
+void game_draw(void);
+
+// ------------ Utils (implemented in util.c) ------------
+int  clampi(int x, int lo, int hi);
+int  rand_range(int lo, int hi);
+void clear_screen(void);
