c7178b48f8a9b50214ccb99eacd9cd6729779ce3
negloir <mr.shamar@gmail.com>
2025-08-12 09:40:07 +0100

    Update types.h

--- include/calico/types.h
+++ include/calico/types.h
@@ -1,12 +1,3 @@
 #pragma once
-/* Minimal calico/types.h stub for CI */
-typedef signed char s8;
-typedef unsigned char u8;
-typedef signed short s16;
-typedef unsigned short u16;
-typedef signed int s32;
-typedef unsigned int u32;
-typedef long long s64;
-typedef unsigned long long u64;
-typedef float f32;
-typedef double f64;
+/* Minimal Calico stub so libnds v2 compiles in CI. */
+#include <stdint.h>
