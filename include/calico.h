0700616d5b43a0e4226d73faa21c1285e8fe6ec9
negloir <mr.shamar@gmail.com>
2025-08-12 10:46:07 +0100

    Update calico.h

--- include/calico.h
+++ include/calico.h
@@ -1,3 +1,2 @@
 #pragma once
-/* Local stub so libnds v2 compiles in CI without the Calico package.
-   This project doesn’t call any Calico APIs. */
+/* Local stub so libnds v2 compiles in CI without the Calico package. */
